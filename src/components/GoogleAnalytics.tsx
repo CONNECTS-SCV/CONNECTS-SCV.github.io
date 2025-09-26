4a5817cd6a25c2c648fb89e372d9cdb89598bbd4
swcha <tjddnr9553@naver.com>
2025-09-26 10:24:08 +0900

    [Refactor] 구글 애널리틱스 재연동

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -6,9 +6,6 @@ const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) {
-    if (typeof window !== 'undefined') {
-      console.warn('Google Analytics ID not found. NEXT_PUBLIC_GA_ID:', process.env.NEXT_PUBLIC_GA_ID);
-    }
     return null;
   }
 
@@ -17,9 +14,6 @@ export function GoogleAnalytics() {
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
-        onLoad={() => {
-          console.log('Google Analytics script loaded with ID:', GA_MEASUREMENT_ID);
-        }}
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
@@ -29,7 +23,6 @@ export function GoogleAnalytics() {
           gtag('config', '${GA_MEASUREMENT_ID}', {
             page_path: window.location.pathname,
           });
-          console.log('Google Analytics initialized with ID: ${GA_MEASUREMENT_ID}');
         `}
       </Script>
     </>
